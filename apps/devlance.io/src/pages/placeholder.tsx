4cf90e1ea341aff9ac273df8ce40890b1f8f33c9
PlutonusDev <joshua@plutonus.dev>
2022-05-07 15:04:34 +0000

    Added second page to test transitioning.

---
+++ apps/devlance.io/src/pages/placeholder.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Layout from "ui/layouts";
+
+const TestPage = () => (
+	<Layout title="Devlance">
+		<p>Placeholder page.</p>
+	</Layout>
+);
+
+export default TestPage;
