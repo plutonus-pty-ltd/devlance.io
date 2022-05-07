43dd2c12591c358bb6fa4c3c350b6dd031085ff2
PlutonusDev <joshua@plutonus.dev>
2022-05-07 15:03:44 +0000

    Removed placeholder tailwind+next.js site, and replaced it with a skeleton for Devlance.

--- apps/devlance.io/src/pages/_app.tsx
+++ apps/devlance.io/src/pages/_app.tsx
@@ -1,10 +1,36 @@
-import * as React from 'react'
+import React from 'react'
 import type { AppProps } from 'next/app'
+import Head from "next/head";
 import '../../styles/globals.css'
+import { AnimatePresence } from "framer-motion";
 
-const MyApp = ({ Component, pageProps }: AppProps) => 
-  // eslint-disable-next-line react/jsx-props-no-spreading
-   <Component {...pageProps} />
+import Header from "ui/components/Header";
+import Footer from "ui/components/Footer";
+import ThemeProvider from "../components/meta/ThemeContext";
 
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <>
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </Head>
+
+    <ThemeProvider>
+      <Header links={[{
+          name: "Home",
+          href: "/"
+        }, {
+          name: "Test",
+          href: "/placeholder"
+      }]}/>
+
+      <AnimatePresence exitBeforeEnter initial={false} onExitComplete={() => window.scrollTo(0, 0)} >
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        <Component {...pageProps} />
+      </AnimatePresence>
+
+      <Footer />
+    </ThemeProvider>
+  </>
+);
 
 export default MyApp
